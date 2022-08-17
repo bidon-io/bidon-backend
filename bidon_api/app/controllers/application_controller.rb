ef124ae17f644fb2f031d58549c2d74465c8d637
minisai <antonjyha@gmail.com>
2022-08-17 16:24:34 +0200

    [BDN-4] Add sentry initializers

--- bidon_api/app/controllers/application_controller.rb
+++ bidon_api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  before_action :set_sentry_context
   before_action :validate_bidon_header!
 
   def validate_bidon_header!
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::API
 
   rescue_from StandardError do |_e|
     render json: { error: { code: 500, message: 'Internal Server Error' } }, status: :internal_server_error
+  end
+
+  def set_sentry_context
+    Sentry.set_extras(params: params.to_unsafe_h, session: session.to_hash)
   end
 end
