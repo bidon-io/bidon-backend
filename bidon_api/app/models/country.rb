4dba945303f2040d37514205e0940927e4313fbe
minisai <antonjyha@gmail.com>
2022-09-01 17:49:03 +0200

    [BDN-12] Update kafka_producer and geocoder

--- bidon_api/app/models/country.rb
+++ bidon_api/app/models/country.rb
@@ -1,7 +1,7 @@
 class Country < Sequel::Model
   def self.find_cached(code)
-    Cache.global.fetch("country_#{code}") do
-      Country.find_by(code:) || Country.find_by(code: 'ZZ')
+    Rails.cache.fetch("country_#{code}") do
+      Country.find(alpha_2_code: code) || Country.find(alpha_2_code: 'ZZ')
     end
   end
 end
--- bidon_api/lib/services/kafka_logger.rb
+++ bidon_api/lib/services/kafka_logger.rb
@@ -2,18 +2,18 @@ module KafkaLogger
   module_function
 
   def log_click(event)
-    $kafka_producer.produce(JSON.dump(event), topic: ENV.fetch('KAFKA_CLICK_TOPIC'))
+    KafkaProducer.produce(JSON.dump(event), topic: ENV.fetch('KAFKA_CLICK_TOPIC'))
   end
 
   def log_finish(event)
-    $kafka_producer.produce(JSON.dump(event), topic: ENV.fetch('KAFKA_FINISH_TOPIC'))
+    KafkaProducer.produce(JSON.dump(event), topic: ENV.fetch('KAFKA_FINISH_TOPIC'))
   end
 
   def log_show(event)
-    $kafka_producer.produce(JSON.dump(event), topic: ENV.fetch('KAFKA_SHOW_TOPIC'))
+    KafkaProducer.produce(JSON.dump(event), topic: ENV.fetch('KAFKA_SHOW_TOPIC'))
   end
 
   def log_stats(event)
-    $kafka_producer.produce(JSON.dump(event), topic: ENV.fetch('KAFKA_STATS_TOPIC'))
+    KafkaProducer.produce(JSON.dump(event), topic: ENV.fetch('KAFKA_STATS_TOPIC'))
   end
 end
