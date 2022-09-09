542614bd2836ccee623da6d9035aa4055401cd73
minisai <antonjyha@gmail.com>
2022-09-09 18:24:34 +0200

    [BDN-16] Add json schemer

--- bidon_api/app/controllers/stats_controller.rb
+++ bidon_api/app/controllers/stats_controller.rb
@@ -2,6 +2,16 @@
 
 class StatsController < ApplicationController
   def create
+    binding.irb
+    schemer.validate(permitted_params)
+
     render_empty_result
+  end
+
+  private
+
+  def schemer
+    schema = Pathname.new(Rails.root.join('json_schema', 'stats.json'))
+    JSONSchemer.schema(schema)
   end
 end
