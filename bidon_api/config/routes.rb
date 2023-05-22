4099fc43d8e12af43b70916f4cf682b370c318ea
Konstantin Ilchenko <k.ilchenko@appodeal.com>
2023-05-22 15:17:48 +0200

    [BDN-XXX] Add endpoint to log rtb requests

--- bidon_api/config/routes.rb
+++ bidon_api/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   post 'config', to: 'config#create'
+  post 'openrtb/:ad_type', to: 'rtb#create'
+  post ':ad_type/openrtb', to: 'rtb#create'
+
   post 'auction/:ad_type', to: 'auction#create'
   post 'stats/:ad_type',   to: 'stats#create'
   post 'click/:ad_type',   to: 'click#create'
