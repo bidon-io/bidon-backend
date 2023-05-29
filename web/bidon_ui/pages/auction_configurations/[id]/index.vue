6d6d33c3c865d1344f905ac3d1d287af64016ddc
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-05-29 11:30:47 +0300

    [BDN-207] Add show page for resource

---
+++ web/bidon_ui/pages/auction_configurations/[id]/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ResourceCard :fields="fields" :resource="resource" />
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const sampleAuctionConfiguration = {
+  id: 1,
+  app: "SampleApp",
+  name: "SampleAuction",
+  adType: "rewarded",
+  priceFloor: 0.1,
+  rounds:
+    '[{"id"=>"ROUND_REWARDED_1", "demands"=>["admob", "unityads", "applovin", "dtexchange"], "timeout"=>15000}, {"id"=>"ROUND_REWARDED_2", "demands"=>["bidmachine"], "timeout"=>15000}]',
+};
+const resource = ref(sampleAuctionConfiguration);
+const fields = [
+  { label: "ID", key: "id" },
+  { label: "App", key: "app", type: "link", link: "/apps/1" },
+  { label: "Name", key: "name" },
+  { label: "Ad type", key: "adType" },
+  { label: "Price floor", key: "priceFloor" },
+  { label: "Rounds", key: "rounds", type: "textarea" },
+];
+</script>
--- web/bidon_ui/pages/auction_configurations/index.vue
+++ web/bidon_ui/pages/auction_configurations/index.vue
@@ -3,7 +3,9 @@
     v-model:selection="selectedConfiguration"
     :value="configurations"
     data-key="id"
-    paginator :rows="12" :rowsPerPageOptions="[12, 24, 36, 48]"
+    paginator
+    :rows="12"
+    :rows-per-page-options="[12, 24, 36, 48]"
     table-style="min-width: 50rem"
   >
     <Column selection-mode="multiple" header-style="width: 3rem"></Column>
@@ -12,6 +14,21 @@
     <Column field="name" header="Name"></Column>
     <Column field="adType" header="AdType"></Column>
     <Column field="priceFloor" header="PriceFloor"></Column>
+    <Column style="width: 10%; min-width: 8rem" body-style="text-align:center">
+      <template #body="slotProps">
+        <div class="flex justify-between">
+          <NuxtLink :key="slotProps.data.id" :to="`/auction_configurations/${slotProps.data.id}`">
+            <i class="pi pi-eye" style="color: slateblue"></i>
+          </NuxtLink>
+          <NuxtLink :key="slotProps.data.id" :to="`/auction_configurations/${slotProps.data.id}/edit`">
+            <i class="pi pi-pencil" style="color: green"></i>
+          </NuxtLink>
+          <NuxtLink :key="slotProps.data.id" :to="`/auction_configurations/${slotProps.data.id}`">
+            <i class="pi pi-trash" style="color: red"></i>
+          </NuxtLink>
+        </div>
+      </template>
+    </Column>
   </DataTable>
 </template>
 
