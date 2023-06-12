42b1fb1a4974cda80372034c5c1b99f3149c52bd
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-06-12 18:21:07 +0300

    [BDN-239] Configure API_URL and CORS

--- web/bidon_ui/constants/API_URL.js
+++ web/bidon_ui/constants/API_URL.js
@@ -1,1 +1,7 @@
-export const API_URL = "http://localhost:1323";
+export let API_URL;
+
+if (import.meta.env.VITE_APP_ENV === "production") {
+  API_URL = "https://bidon-go.appodeal.com/";
+} else {
+  API_URL = "http://localhost:1323";
+}
