413038c91d6394ef5544e25919ec5ef261690dce
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-06-16 14:24:15 +0300

    [BDN-XXX Fix GoBackBtn

--- web/bidon_ui/pages/auction_configurations/[id]/edit.vue
+++ web/bidon_ui/pages/auction_configurations/[id]/edit.vue
@@ -2,7 +2,7 @@
   <Toast />
   <PageContainer>
     <NavigationContainer>
-      <GoBackButton :path="resourcesPath" />
+      <GoBackButton" />
     </NavigationContainer>
     <AuctionConfigurationForm v-if="isReady" :value="resource" @submit="handleSubmit" />
   </PageContainer>
