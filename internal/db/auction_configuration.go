4fa399d7917fdb25102afa56f4f9a27a01bf8474
minisai <antonjyha@gmail.com>
2023-06-16 12:17:41 +0200

    [BDN-243] Update callback

---
+++ internal/db/auction_configuration.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"errors"
+	"gorm.io/gorm"
+)
+
+func (ac *AuctionConfiguration) BeforeSave(tx *gorm.DB) (err error) {
+	// Check if the combination of app_id, ad_type, and segment_id is already taken
+	var count int64
+
+	query := tx.Model(&AuctionConfiguration{}).
+		Where("app_id = ? AND ad_type = ?", ac.AppID, ac.AdType)
+
+	if ac.SegmentID == nil {
+		query = query.Where("segment_id IS NULL")
+	} else {
+		query = query.Where("segment_id = ?", ac.SegmentID)
+	}
+
+	query = query.Not(ac.ID).Count(&count)
+
+	if query.Error != nil {
+		return query.Error
+	}
+
+	if count > 0 {
+		return errors.New("the combination of app_id, ad_type, and segment_id already exists")
+	}
+
+	return nil
+}
