ae15bab9465a44bde3c1a195003a63869cd087f3
minisai <antonjyha@gmail.com>
2023-07-07 17:18:18 +0200

    [BDN-275] Implement OK test

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/labstack/echo/v4 v4.10.2
 	github.com/oschwald/maxminddb-golang v1.11.0
 	github.com/shopspring/decimal v1.3.1
+	github.com/stretchr/testify v1.8.4
 	github.com/uptrace/opentelemetry-go-extra/otelgorm v0.2.2
 	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.42.0
 	go.opentelemetry.io/otel v1.16.0
@@ -22,6 +23,7 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -33,10 +35,13 @@ require (
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/uptrace/opentelemetry-go-extra/otelsql v0.2.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
@@ -49,4 +54,5 @@ require (
 	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/sdkapi/auction_handler_test.go
+++ internal/sdkapi/auction_handler_test.go
@@ -1,18 +1,21 @@
-package sdkapi
+package sdkapi_test
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/bidon-io/bidon-backend/config"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
-	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/bidon-io/bidon-backend/internal/ad"
 	"github.com/bidon-io/bidon-backend/internal/adapter"
 	"github.com/bidon-io/bidon-backend/internal/auction"
 	auctionmocks "github.com/bidon-io/bidon-backend/internal/auction/mocks"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi"
 	"github.com/bidon-io/bidon-backend/internal/sdkapi/geocoder"
 	sdkapimocks "github.com/bidon-io/bidon-backend/internal/sdkapi/mocks"
 	"github.com/bidon-io/bidon-backend/internal/sdkapi/schema"
@@ -20,21 +23,10 @@ import (
 	segmentmocks "github.com/bidon-io/bidon-backend/internal/segment/mocks"
 )
 
-func TestAuctionHandler_Handle(t *testing.T) {
-	// Create a new Echo instance
-	e := echo.New()
-	app := App{ID: 1}
-	appFetcher := &sdkapimocks.AppFetcherMock{
-		FetchFunc: func(ctx context.Context, appKey string, appBundle string) (App, error) {
-			return app, nil
-		},
-	}
-	gcoder := &sdkapimocks.GeocoderMock{
-		LookupFunc: func(ctx context.Context, ipString string) (geocoder.GeoData, error) {
-			return geocoder.GeoData{}, nil
-		},
-	}
-	config := &auction.Config{
+func TestAuctionHandler_OK(t *testing.T) {
+	app := sdkapi.App{ID: 1}
+	geodata := geocoder.GeoData{CountryCode: "US"}
+	auctionConfig := &auction.Config{
 		ID: 1,
 		Rounds: []auction.RoundConfig{
 			{
@@ -69,12 +61,22 @@ func TestAuctionHandler_Handle(t *testing.T) {
 		{ID: "test", PriceFloor: 0.1, AdUnitID: "test_id"},
 	}
 	segments := []segment.Segment{
-		{ID: 1, Filters: []segment.Filter{{Type: "country", Name: "country", Operator: "in", Values: []string{"US", "UK"}}}},
+		{ID: 1, Filters: []segment.Filter{{Type: "country", Name: "country", Operator: "IN", Values: []string{"US", "UK"}}}},
 	}
 
+	appFetcher := &sdkapimocks.AppFetcherMock{
+		FetchFunc: func(ctx context.Context, appKey string, appBundle string) (sdkapi.App, error) {
+			return app, nil
+		},
+	}
+	gcoder := &sdkapimocks.GeocoderMock{
+		LookupFunc: func(ctx context.Context, ipString string) (geocoder.GeoData, error) {
+			return geodata, nil
+		},
+	}
 	configMatcher := &auctionmocks.ConfigMatcherMock{
 		MatchFunc: func(ctx context.Context, appID int64, adType ad.Type, segmentID int64) (*auction.Config, error) {
-			return config, nil
+			return auctionConfig, nil
 		},
 	}
 	lineItemsMatcher := &auctionmocks.LineItemsMatcherMock{
@@ -96,8 +98,9 @@ func TestAuctionHandler_Handle(t *testing.T) {
 	}
 
 	// Create a new AuctionHandler instance
-	handler := &AuctionHandler{
-		BaseHandler: &BaseHandler[schema.AuctionRequest, *schema.AuctionRequest]{
+
+	handler := &sdkapi.AuctionHandler{
+		BaseHandler: &sdkapi.BaseHandler[schema.AuctionRequest, *schema.AuctionRequest]{
 			AppFetcher: appFetcher,
 			Geocoder:   gcoder,
 		},
@@ -105,13 +108,93 @@ func TestAuctionHandler_Handle(t *testing.T) {
 		SegmentMatcher: segmentMatcher,
 	}
 
+	// Create a sample request body
+
+	requestJson := `{
+		"adapters": {
+			"admob": {
+				"version": "0.2.1.11",
+				"sdk_version": "21.5.0"
+			},
+			"bidmachine": {
+				"version": "0.2.1.12",
+				"sdk_version": "2.1.13"
+			},
+			"dtexchange": {
+				"version": "0.2.1.11",
+				"sdk_version": "8.2.3"
+			},
+			"unityads": {
+				"version": "0.2.1.11",
+				"sdk_version": "4.5.0"
+			}
+		},
+		"device": {
+			"connection_type": "WIFI",
+			"model": "Google Pixel 3",
+			"hwv": "blueline",
+			"h": 2028,
+			"js": 1,
+			"language": "en_US",
+			"make": "Google",
+			"os": "android",
+			"os_api_level": "31",
+			"osv": "12",
+			"ppi": 440,
+			"pxratio": 2.75,
+			"type": "PHONE",
+			"ua": "Mozilla\/5.0 (Linux; Android 12; Pixel 3 Build\/SP1A.210812.016.C1; wv) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/114.0.5735.131 Mobile Safari\/537.36",
+			"w": 1080
+		},
+		"app": {
+			"bundle": "com.newpubco.merge",
+			"framework": "unity",
+			"framework_version": "2020.3.48f1",
+			"key": "23f1181b191f34a04f4a74840c09d116028a98ed3721de6b",
+			"version": "2.6.42"
+		},
+		"token": "{}",
+		"session": {
+			"battery": 100,
+			"cpu_usage": 0.7714286,
+			"id": "d0b9ec0e-60e9-4501-bcbc-67712562dab9",
+			"launch_monotonic_ts": 68406545,
+			"launch_ts": 1687863971253,
+			"memory_warnings_monotonic_ts": [],
+			"memory_warnings_ts": [],
+			"start_monotonic_ts": 68406545,
+			"monotonic_ts": 68471084,
+			"ram_size": 3753299968,
+			"ram_used": 413565952,
+			"start_ts": 1687863971253,
+			"storage_free": 30742986752,
+			"storage_used": 24636485632,
+			"ts": 1687864035792
+		},
+		"user": {
+			"idg": "8020a0a6-5e55-4b8f-af9a-7b01a9aad4fc",
+			"coppa": false,
+			"idfa": "0d95325a-71b7-4334-8816-9e2935ec0eef",
+			"tracking_authorization_status": "AUTHORIZED"
+		},
+		"ext": "{\"appodeal_segment_id\":20194,\"appodeal_session_id\":\"c6c5ce3e-b8cd-492c-ae7f-15f0e679883b\",\"appodeal_token\":{\"id\":1687857767,\"last_init\":1687863971,\"signature\":\"RXVQdjhUdUE4Q1UyZkVKVkdYN3hKMGU0YzREa29JNGFleDk3SFg0NmlmS2FuWDF0bFVVSUpZWVVJZFE3ZUIvZy0tRHlmSFNNT2Q3M01MQU9zbWc5L2o4QT09--a9b3ccac217e3a2f4be477f2f15dbb5785c0747f\"},\"ext\":{\"sample_key\":\"sample_value\"}}",
+		"ad_object": {
+			"auction_id": "fe91db30-013d-40ff-b9b8-2e99519dd5e0",
+			"interstitial": {},
+			"orientation": "PORTRAIT",
+			"pricefloor": 0.66
+		}
+	}`
+
 	// Create a new HTTP request
-	req := httptest.NewRequest(http.MethodPost, "/", nil)
+	req := httptest.NewRequest(http.MethodPost, "/auction/interstitial", strings.NewReader(requestJson))
+	req.Header.Set("Content-Type", "application/json")
 
 	// Create a new HTTP response recorder
 	rec := httptest.NewRecorder()
 
-	// Create a new Echo context
+	// Create a new Echo instance and context
+	e := config.Echo("sdkapi-test", nil)
 	c := e.NewContext(req, rec)
 
 	// Call the Handle method
@@ -123,6 +206,78 @@ func TestAuctionHandler_Handle(t *testing.T) {
 	// Assert that the response status code is HTTP 200 OK
 	assert.Equal(t, http.StatusOK, rec.Code)
 
-	// Assert any other response expectations based on the implementation
-	// and the specific scenario you want to test
+	expectedResponseJson := `{
+      "auction_configuration_id": 1,
+      "external_win_notifications": false,
+      "rounds": [
+        {
+          "id": "ROUND_1",
+          "demands": [
+            "bidmachine"
+          ],
+          "bidding": [],
+          "timeout": 15000
+        },
+        {
+          "id": "ROUND_2",
+          "demands": [
+            "unityads"
+          ],
+          "bidding": [
+            "bidmachine"
+          ],
+          "timeout": 15000
+        },
+        {
+          "id": "ROUND_4",
+          "demands": [
+            "unityads"
+          ],
+          "bidding": [],
+          "timeout": 15000
+        },
+        {
+          "id": "ROUND_5",
+          "demands": [],
+          "bidding": [
+            "bidmachine"
+          ],
+          "timeout": 15000
+        }
+      ],
+      "line_items": [
+        {
+          "id": "test",
+          "pricefloor": 0.1,
+          "ad_unit_id": "test_id"
+        }
+      ],
+      "segment": {
+        "id": "1"
+      },
+      "token": "{}",
+      "pricefloor": 0.66,
+      "auction_id": "fe91db30-013d-40ff-b9b8-2e99519dd5e0"
+    }`
+
+	var actualResponse interface{}
+	var exptectedResponse interface{}
+
+	err = json.Unmarshal([]byte(rec.Body.String()), &actualResponse)
+	if err != nil {
+		t.Fatalf("Failed to parse JSON1: %s", err)
+	}
+
+	err = json.Unmarshal([]byte(expectedResponseJson), &exptectedResponse)
+	if err != nil {
+		t.Fatalf("Failed to parse JSON2: %s", err)
+	}
+
+	assert.Equal(t, actualResponse, exptectedResponse)
 }
+
+// TODO: add tests for:
+// test ErrNoAdsFound
+// test resolve request error
+// test Validate error
+// test AppFetcher error
