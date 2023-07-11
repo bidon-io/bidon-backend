4a146cbbe39c51c510f6b1a38a1e2742c224c8ed
Ilya Demukh <i.demukh@appodeal.com>
2023-07-11 18:05:03 +0200

    Add BatchMaxBytes option

--- config/kafka.go
+++ config/kafka.go
@@ -19,16 +19,31 @@ type KafkaConfig struct {
 func Kafka() (conf KafkaConfig, err error) {
 	seeds := strings.Split(os.Getenv("KAFKA_BROKERS_LIST"), ", ")
 	clientID := os.Getenv("KAFKA_CLIENT_ID")
-	linger, err := strconv.Atoi(os.Getenv("KAFKA_DELIVERY_INTERVAL"))
-	if err != nil {
-		return conf, fmt.Errorf("invalid KAFKA_DELIVERY_INTERVAL: %v", err)
-	}
 
 	conf.ClientOpts = []kgo.Opt{
 		kgo.AllowAutoTopicCreation(),
 		kgo.SeedBrokers(seeds...),
 		kgo.ClientID(clientID),
-		kgo.ProducerLinger(time.Second * time.Duration(linger)),
+	}
+
+	batchMaxBytes := os.Getenv("KAFKA_BATCH_MAX_BYTES")
+	if batchMaxBytes != "" {
+		value, err := strconv.Atoi(batchMaxBytes)
+		if err != nil {
+			return conf, fmt.Errorf("invalid KAFKA_BATCH_MAX_BYTES: %v", err)
+		}
+
+		conf.ClientOpts = append(conf.ClientOpts, kgo.ProducerBatchMaxBytes(int32(value)))
+	}
+
+	linger := os.Getenv("KAFKA_DELIVERY_INTERVAL")
+	if linger != "" {
+		value, err := strconv.Atoi(linger)
+		if err != nil {
+			return conf, fmt.Errorf("invalid KAFKA_DELIVERY_INTERVAL: %v", err)
+		}
+
+		conf.ClientOpts = append(conf.ClientOpts, kgo.ProducerLinger(time.Second*time.Duration(value)))
 	}
 
 	configTopic := os.Getenv("KAFKA_CONFIG_TOPIC")
