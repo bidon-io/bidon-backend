5832ca91179274d8c1ce408505c31e5ff8932f38
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-07-27 15:35:19 +0200

    [BDN-305] Add ParseBids spec

--- internal/bidding/adapters/bigoads/bigoads_test.go
+++ internal/bidding/adapters/bigoads/bigoads_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/bidon-io/bidon-backend/internal/ad"
 	"github.com/bidon-io/bidon-backend/internal/adapter"
+	"github.com/bidon-io/bidon-backend/internal/bidding/adapters"
 	"github.com/bidon-io/bidon-backend/internal/bidding/adapters/bigoads"
 	"github.com/bidon-io/bidon-backend/internal/sdkapi/schema"
 	"github.com/google/go-cmp/cmp"
@@ -24,8 +25,33 @@ type createRequestTestOutput struct {
 	Err     []error
 }
 
+type ParseBidsTestParams struct {
+	DemandsResponse adapters.DemandResponse
+}
+
+type ParseBidsTestOutput struct {
+	DemandResponse adapters.DemandResponse
+	Err            error
+}
+
 func ptr[T any](t T) *T {
 	return &t
+}
+
+func compareErrors(x, y error) bool {
+	if x == nil || y == nil {
+		return x == y
+	}
+	return x.Error() == y.Error()
+}
+
+func buildAdapter() bigoads.BigoAdsAdapter {
+	return bigoads.BigoAdsAdapter{
+		SellerID: "1",
+		Endpoint: "https://bigoads.com",
+		AppID:    "10182906",
+		TagID:    "10182906-10192212",
+	}
 }
 
 func buildBaseRequest() openrtb2.BidRequest {
@@ -102,7 +128,7 @@ func buildWantRequest(imp openrtb2.Imp) openrtb2.BidRequest {
 	return request
 }
 
-func TestBigoAds_CreateRequestTest(t *testing.T) {
+func TestBigoAds_CreateRequest(t *testing.T) {
 	testCases := []struct {
 		name   string
 		params createRequestTestParams
@@ -204,13 +230,7 @@ func TestBigoAds_CreateRequestTest(t *testing.T) {
 		},
 	}
 
-	adapter := &bigoads.BigoAdsAdapter{
-		SellerID: "1",
-		Endpoint: "https://bigoads.com",
-		AppID:    "10182906",
-		TagID:    "10182906-10192212",
-	}
-
+	adapter := buildAdapter()
 	for _, tC := range testCases {
 		request, err := adapter.CreateRequest(tC.params.BaseBidRequest, tC.params.Br)
 		if err == nil {
@@ -220,10 +240,106 @@ func TestBigoAds_CreateRequestTest(t *testing.T) {
 			Request: request,
 			Err:     err,
 		}
-		if diff := cmp.Diff(tC.want, got, cmp.Comparer(func(x, y error) bool {
-			return x.Error() == y.Error()
-		})); diff != "" {
+		if diff := cmp.Diff(tC.want, got, cmp.Comparer(compareErrors)); diff != "" {
 			t.Errorf("%s: adapter.CreateRequest(ctx, %v, %v) mismatch (-want, +got):\n%s", tC.name, tC.params.BaseBidRequest, tC.params.Br, diff)
+		}
+	}
+}
+
+func TestBigoAds_ParseBids(t *testing.T) {
+	rawResponse := `{
+		"id": "47611e59-e05b-4e1e-9074-5a65eb4501e4",
+		"seatbid": [
+			{
+				"bid": [
+					{
+						"id": "0",
+						"impid": "6579ca7b-7e2c-48b6-8915-46efa6530fb5",
+						"price": 1.5,
+						"nurl": "https://api.gov-static.tech/Ad/AdxEvent?sid=0&sslot=10182906-10163778&adtype=4",
+						"lurl": "https://api.gov-static.tech/Ad/AdxEvent?sid=0&sslot=10182906-10163778",
+						"adm": "0692d0a0efdbd5bd470dafea742cef6a1f6b840c5c83240e165bc33a038b3d5487e25a52",
+						"adid": "Bigoad5e0471131b8a4e3c",
+						"crid": "e2d42134881d5b45134f3cf77989dec7"
+					}
+				]
+			}
+		]
+	}`
+	adapter := buildAdapter()
+
+	testCases := []struct {
+		name   string
+		params ParseBidsTestParams
+		want   ParseBidsTestOutput
+	}{
+		{
+			name: "ParseBids Success",
+			params: ParseBidsTestParams{
+				DemandsResponse: adapters.DemandResponse{
+					Status:      200,
+					RawResponse: rawResponse,
+				},
+			},
+			want: ParseBidsTestOutput{
+				DemandResponse: adapters.DemandResponse{
+					Status:      200,
+					RawResponse: rawResponse,
+					Bid: &adapters.BidDemandResponse{
+						ID:       "0",
+						ImpID:    "6579ca7b-7e2c-48b6-8915-46efa6530fb5",
+						Price:    1.5,
+						Payload:  "0692d0a0efdbd5bd470dafea742cef6a1f6b840c5c83240e165bc33a038b3d5487e25a52",
+						DemandID: "bigoads",
+						AdID:     "Bigoad5e0471131b8a4e3c",
+						LURL:     "https://api.gov-static.tech/Ad/AdxEvent?sid=0&sslot=10182906-10163778",
+						NURL:     "https://api.gov-static.tech/Ad/AdxEvent?sid=0&sslot=10182906-10163778&adtype=4",
+					},
+				},
+				Err: nil,
+			},
+		},
+		{
+			name: "ParseBids Bad Request",
+			params: ParseBidsTestParams{
+				DemandsResponse: adapters.DemandResponse{
+					Status:      400,
+					RawResponse: rawResponse,
+				},
+			},
+			want: ParseBidsTestOutput{
+				DemandResponse: adapters.DemandResponse{
+					Status:      400,
+					RawResponse: rawResponse,
+				},
+				Err: errors.New("unauthorized request: 400"),
+			},
+		},
+		{
+			name: "ParseBids No Conten",
+			params: ParseBidsTestParams{
+				DemandsResponse: adapters.DemandResponse{
+					Status:      204,
+					RawResponse: rawResponse,
+				},
+			},
+			want: ParseBidsTestOutput{
+				DemandResponse: adapters.DemandResponse{
+					Status:      204,
+					RawResponse: rawResponse,
+				},
+				Err: nil,
+			},
+		},
+	}
+	for _, tC := range testCases {
+		response, err := adapter.ParseBids(&tC.params.DemandsResponse)
+		got := ParseBidsTestOutput{
+			DemandResponse: *response,
+			Err:            err,
+		}
+		if diff := cmp.Diff(tC.want, got, cmp.Comparer(compareErrors)); diff != "" {
+			t.Errorf("%s: adapter.ParseBids(ctx, %v) mismatch (-want, +got):\n%s", tC.name, tC.params.DemandsResponse, diff)
 		}
 	}
 }
