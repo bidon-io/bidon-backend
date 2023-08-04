d33e027311fb4044578c4259f08efe5018abc249
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-08-04 12:53:46 +0200

    [BDN-327] Highlight choosen section

--- web/bidon_ui/components/layouts/SidebarNavigation.vue
+++ web/bidon_ui/components/layouts/SidebarNavigation.vue
@@ -4,7 +4,12 @@
       v-for="item in menuItems"
       :key="item.name"
       :to="item.link"
-      class="flex items-center mt-4 px-6 py-2 text-gray-600 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+      :class="[
+        'flex items-center mt-4 px-6 py-2 text-gray-600 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100',
+        route.path === item.link
+          ? 'bg-gray-700 bg-opacity-25 text-blue-100'
+          : '',
+      ]"
     >
       <span>{{ item.name }}</span>
     </NuxtLink>
@@ -20,4 +25,5 @@ const menuItems = [
   { name: "Line Items", link: "/line_items" },
   { name: "Segments", link: "/segments" },
 ];
+const route = useRoute();
 </script>
