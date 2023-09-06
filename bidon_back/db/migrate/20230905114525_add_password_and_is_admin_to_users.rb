a283e6dc7b712849904e0edc1d58d95d6384c23b
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-09-06 10:54:39 +0200

    fixup! [BDN-370] Create/Update user with Password and IsAdmin

--- bidon_back/db/migrate/20230905114525_add_password_and_is_admin_to_users.rb
+++ bidon_back/db/migrate/20230905114525_add_password_and_is_admin_to_users.rb
@@ -1,6 +1,6 @@
 class AddPasswordAndIsAdminToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :is_admin, :boolean, default: false
+    add_column :users, :is_admin, :boolean, null: false, default: false
 
     add_column :users, :password_hash, :string
     User.update_all(password_hash: 'password') # rubocop:disable Rails/SkipsModelValidations
