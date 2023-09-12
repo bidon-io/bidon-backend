3cfebf3ac438a8be71b350e99f5ffb24058ac5c7
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-09-12 12:07:56 +0200

    fixup! [BDN-376] Apply permissions on UI

--- web/bidon_ui/components/layouts/SidebarNavigation.vue
+++ web/bidon_ui/components/layouts/SidebarNavigation.vue
@@ -22,16 +22,5 @@ import { useAuthStore } from "@/stores/AuthStore";
 const { getResourcesPermissions } = useAuthStore();
 const { data: menuItems } = useAsyncData(getResourcesPermissions);
 
-// const menuItems = [
-//   { name: "App Demand Profile", link: "/app_demand_profiles" },
-//   { name: "Apps", link: "/apps" },
-//   { name: "Auction Configurations", link: "/auction_configurations" },
-//   { name: "Demand Source Accounts", link: "/demand_source_accounts" },
-//   { name: "Line Items", link: "/line_items" },
-//   { name: "Segments", link: "/segments" },
-//   { name: "Demand Sources", link: "/demand_sources" },
-//   { name: "Country", link: "/countries" },
-//   { name: "User", link: "/users" },
-// ];
 const route = useRoute();
 </script>
