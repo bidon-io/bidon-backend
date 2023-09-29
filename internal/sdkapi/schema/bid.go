d1abfb2c684a88ce956d6c92691c64e7f23adb16
Konstantin Ilchenko <k.ilchenko@appodeal.com>
2023-09-30 01:56:40 +0200

    [BDN-373] Add bit_type to stats

--- internal/sdkapi/schema/bid.go
+++ internal/sdkapi/schema/bid.go
@@ -13,9 +13,14 @@ type Bid struct {
 	AdUnitID                string                `json:"ad_unit_id"`
 	LineItemUID             string                `json:"line_item_uid"`
 	ECPM                    float64               `json:"ecpm" validate:"required"`
+	BidType                 string                `json:"bid_type" validate:"omitempty,oneof=rtb cpm"`
 	Banner                  *BannerAdObject       `json:"banner"`
 	Interstitial            *InterstitialAdObject `json:"interstitial"`
 	Rewarded                *RewardedAdObject     `json:"rewarded"`
+}
+
+func (b Bid) IsBidding() bool {
+	return b.BidType == "rtb"
 }
 
 func (b Bid) Map() map[string]any {
@@ -35,10 +40,12 @@ func (b Bid) Map() map[string]any {
 		"imp_id":                    b.ImpID,
 		"demand_id":                 b.DemandID,
 		"round_id":                  b.RoundID,
-		"round_idx":                 b.RoundIndex,
+		"round_number":              b.RoundIndex,
 		"ad_unit_id":                b.AdUnitID,
 		"line_item_uid":             lineItemUID,
 		"ecpm":                      b.ECPM,
+		"bid_type":                  b.BidType,
+		"bidding":                   b.IsBidding(),
 	}
 
 	if b.Banner != nil {
