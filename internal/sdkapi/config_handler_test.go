c6d4b56562b0d2452465a924398359f4ec200fc7
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-10-13 18:22:19 +0200

    [BDN-229] Add /config specs

---
+++ internal/sdkapi/config_handler_test.go
@@ -0,0 +1,142 @@
+package sdkapi_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/bidon-io/bidon-backend/config"
+	"github.com/bidon-io/bidon-backend/internal/adapter"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/event"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/event/engine"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/geocoder"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/mocks"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/schema"
+	"github.com/bidon-io/bidon-backend/internal/segment"
+	segmentmocks "github.com/bidon-io/bidon-backend/internal/segment/mocks"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+)
+
+func SetupConfigHandler() sdkapi.ConfigHandler {
+	app := sdkapi.App{ID: 1}
+	geodata := geocoder.GeoData{CountryCode: "US"}
+	sgmnt := segment.Segment{
+		ID:      1,
+		UID:     "1",
+		Filters: []segment.Filter{segment.Filter{Type: "country", Operator: "IN", Values: []string{"US"}}},
+	}
+
+	appFetcher := &mocks.AppFetcherMock{
+		FetchFunc: func(ctx context.Context, appKey string, appBundle string) (sdkapi.App, error) {
+			return app, nil
+		},
+	}
+	geocoder := &mocks.GeocoderMock{
+		LookupFunc: func(ctx context.Context, ipString string) (geocoder.GeoData, error) {
+			return geodata, nil
+		},
+	}
+	segmentFetcher := &segmentmocks.FetcherMock{
+		FetchFunc: func(ctx context.Context, appID int64) ([]segment.Segment, error) {
+			return []segment.Segment{sgmnt}, nil
+		},
+	}
+	segmentMatcher := &segment.Matcher{
+		Fetcher: segmentFetcher,
+	}
+	adapterInitConfigsFetcher := &mocks.AdapterInitConfigsFetcherMock{
+		FetchAdapterInitConfigsFunc: func(ctx context.Context, appID int64, adapterKeys []adapter.Key) ([]sdkapi.AdapterInitConfig, error) {
+			return []sdkapi.AdapterInitConfig{
+				&sdkapi.AdmobInitConfig{
+					AppID: fmt.Sprintf("admob_app_%d", app.ID),
+				},
+				&sdkapi.ApplovinInitConfig{
+					AppKey: "applovin",
+					SDKKey: "applovin",
+				},
+				&sdkapi.BidmachineInitConfig{
+					SellerID:        "1",
+					Endpoint:        "x.appbaqend.com",
+					MediationConfig: []string{"one", "two"},
+				},
+				&sdkapi.DTExchangeInitConfig{
+					AppID: fmt.Sprintf("dtexchange_app_%d", app.ID),
+				},
+				&sdkapi.MetaInitConfig{
+					AppID:     fmt.Sprintf("meta_app_%d", app.ID),
+					AppSecret: fmt.Sprintf("meta_app_%d_secret", app.ID),
+				},
+				&sdkapi.MintegralInitConfig{
+					AppID:  fmt.Sprintf("mintegral_app_%d", app.ID),
+					AppKey: "mintegral",
+				},
+			}, nil
+		},
+	}
+
+	return sdkapi.ConfigHandler{
+		BaseHandler: &sdkapi.BaseHandler[schema.ConfigRequest, *schema.ConfigRequest]{
+			AppFetcher: appFetcher,
+			Geocoder:   geocoder,
+		},
+		EventLogger:               &event.Logger{Engine: &engine.Log{}},
+		SegmentMatcher:            segmentMatcher,
+		AdapterInitConfigsFetcher: adapterInitConfigsFetcher,
+	}
+}
+
+func TestConfigHandler_Handle(t *testing.T) {
+	rec := httptest.NewRecorder()
+	reqBody, err := os.ReadFile("testdata/config/valid_request.json")
+	if err != nil {
+		t.Fatalf("Error reading request file: %v", err)
+	}
+
+	// Create a new HTTP request
+	req := httptest.NewRequest(http.MethodPost, "/config", strings.NewReader(string(reqBody)))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	handler := SetupConfigHandler()
+
+	e := config.Echo()
+	c := e.NewContext(req, rec)
+
+	err = handler.Handle(c)
+	if err != nil {
+		t.Fatalf("Error handling request: %v", err)
+	}
+	if rec.Code != http.StatusOK {
+		t.Fatalf("Expected status code %d, got %d", http.StatusOK, rec.Code)
+	}
+}
+
+func TestConfigHandler_Handle_InvalidRequest(t *testing.T) {
+	rec := httptest.NewRecorder()
+	reqBody, err := os.ReadFile("testdata/config/invalid_request.json")
+	if err != nil {
+		t.Fatalf("Error reading request file: %v", err)
+	}
+
+	// Create a new HTTP request
+	req := httptest.NewRequest(http.MethodPost, "/config", strings.NewReader(string(reqBody)))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	handler := SetupConfigHandler()
+
+	e := config.Echo()
+	c := e.NewContext(req, rec)
+
+	err = handler.Handle(c)
+	if err == nil {
+		t.Fatalf("Expected error, got nil")
+	}
+
+	echoError, ok := err.(*echo.HTTPError)
+	if ok && echoError.Code != http.StatusUnprocessableEntity {
+		t.Fatalf("Expected status code %d, got %d", http.StatusBadRequest, rec.Code)
+	}
+}
