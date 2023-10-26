daeb3cda70aafffa6dc9e4a9dacd8f64149c47aa
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2023-10-26 16:26:39 +0200

    [BDN-467] Add new fields to /stats request

--- internal/sdkapi/stats_handler.go
+++ internal/sdkapi/stats_handler.go
@@ -84,11 +84,12 @@ func (h *StatsHandler) sendEvents(c echo.Context, req *request[schema.StatsReque
 		RoundID:                 stats.Result.RoundID,
 		RoundNumber:             0,
 		ImpID:                   "",
-		DemandID:                stats.Result.WinnerID,
+		DemandID:                stats.Result.GetWinnerDemandID(),
 		AdUnitID:                0,
-		LineItemUID:             0,
+		LineItemUID:             int64(stats.Result.GetWinnerAdUnitUID()),
+		LineItemLabel:           stats.Result.WinnerAdUnitLabel,
 		AdUnitCode:              "",
-		Ecpm:                    stats.Result.ECPM,
+		Ecpm:                    stats.Result.GetWinnerPrice(),
 		PriceFloor:              statsPriceFloor,
 	}
 	statsRequestEvent := event.NewRequest(&req.raw.BaseRequest, adRequestParams, req.geoData)
@@ -106,11 +107,12 @@ func (h *StatsHandler) sendEvents(c echo.Context, req *request[schema.StatsReque
 			RoundID:                 round.ID,
 			RoundNumber:             roundNumber,
 			ImpID:                   "",
-			DemandID:                round.WinnerID,
+			DemandID:                round.GetWinnerDemandID(),
 			AdUnitID:                0,
-			LineItemUID:             0,
+			LineItemUID:             int64(round.GetWinnerAdUnitUID()),
+			LineItemLabel:           round.WinnerAdUnitLabel,
 			AdUnitCode:              "",
-			Ecpm:                    round.WinnerECPM,
+			Ecpm:                    round.GetWinnerPrice(),
 			PriceFloor:              round.PriceFloor,
 		}
 		if round.WinnerID != "" {
@@ -124,11 +126,6 @@ func (h *StatsHandler) sendEvents(c echo.Context, req *request[schema.StatsReque
 		})
 
 		for _, demand := range round.Demands {
-			lineItemUID, err := strconv.Atoi(demand.LineItemUID)
-			if err != nil {
-				lineItemUID = 0
-			}
-
 			adRequestParams = event.AdRequestParams{
 				EventType:               "demand_request",
 				AdType:                  string(req.raw.AdType),
@@ -141,9 +138,10 @@ func (h *StatsHandler) sendEvents(c echo.Context, req *request[schema.StatsReque
 				ImpID:                   "",
 				DemandID:                demand.ID,
 				AdUnitID:                0,
-				LineItemUID:             int64(lineItemUID),
+				LineItemUID:             int64(demand.GetAdUnitUID()),
+				LineItemLabel:           demand.AdUnitLabel,
 				AdUnitCode:              demand.AdUnitID,
-				Ecpm:                    demand.ECPM,
+				Ecpm:                    demand.GetPrice(),
 				PriceFloor:              round.PriceFloor,
 				Bidding:                 false,
 			}
@@ -166,9 +164,10 @@ func (h *StatsHandler) sendEvents(c echo.Context, req *request[schema.StatsReque
 				ImpID:                   "",
 				DemandID:                bid.ID,
 				AdUnitID:                0,
-				LineItemUID:             0,
+				LineItemUID:             int64(bid.GetAdUnitUID()),
+				LineItemLabel:           bid.AdUnitLabel,
 				AdUnitCode:              "",
-				Ecpm:                    bid.ECPM,
+				Ecpm:                    bid.GetPrice(),
 				PriceFloor:              round.PriceFloor,
 				Bidding:                 true,
 			}
