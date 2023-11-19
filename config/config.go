93576d0b9b936571fa9e6f3966dcecfe3d3d7f13
Ilya Demukh <i.demukh@appodeal.com>
2023-11-19 11:36:40 +0100

    [BDN-528] Add models generation from schema

--- config/config.go
+++ config/config.go
@@ -1,19 +1,40 @@
 // Package config provides configuration for different parts of Bidon services, that is shared between them
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 var Env = getEnv()
 
 const (
 	ProdEnv = "production"
 	DevEnv  = "development"
+	TestEnv = "test"
 )
 
-func getEnv() string {
-	if os.Getenv("ENVIRONMENT") == ProdEnv {
-		return ProdEnv
+func LoadEnvFile() {
+	var err error
+	if Env == TestEnv {
+		err = godotenv.Load(".env.test")
+	} else {
+		err = godotenv.Load()
 	}
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
 
-	return DevEnv
+func getEnv() string {
+	switch env := os.Getenv("ENVIRONMENT"); env {
+	case ProdEnv:
+		return ProdEnv
+	case TestEnv:
+		return TestEnv
+	default:
+		return DevEnv
+	}
 }
