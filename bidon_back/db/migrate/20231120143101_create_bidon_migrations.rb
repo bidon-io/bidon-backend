50fc8b03d1489401e277eca6b2c322e0cc651195
Ilya Demukh <i.demukh@appodeal.com>
2023-11-20 15:58:17 +0100

    [BDN-540] Sync migration systems

---
+++ bidon_back/db/migrate/20231120143101_create_bidon_migrations.rb
@@ -0,0 +1,23 @@
+class CreateBidONMigrations < ActiveRecord::Migration[7.0]
+  def up
+    safety_assured do
+      execute <<~SQL
+        CREATE TABLE bidon_migrations (
+          id serial PRIMARY KEY,
+          version_id bigint NOT NULL,
+          is_applied boolean NOT NULL,
+          tstamp timestamp DEFAULT now()
+        );
+        INSERT INTO bidon_migrations (version_id, is_applied) VALUES (20231120143101, true);
+      SQL
+    end
+  end
+
+  def down
+    safety_assured do
+      execute <<~SQL
+        DROP TABLE bidon_migrations;
+      SQL
+    end
+  end
+end
