c43124b71d3761faadf513ca83767027aa354895
minisai <antonjyha@gmail.com>
2024-06-12 17:37:40 +0200

    [BDN-645] Fix tests

--- internal/sdkapi/v2/apihandlers/auction_handler_test.go
+++ internal/sdkapi/v2/apihandlers/auction_handler_test.go
@@ -103,7 +103,7 @@ func testHelperAuctionV2Handler(t *testing.T) *apihandlers.AuctionHandler {
 		},
 	}
 	configFetcher := &handlersmocks.ConfigFetcherMock{
-		MatchFunc: func(ctx context.Context, appID int64, adType ad.Type, segmentID int64) (*auction.Config, error) {
+		MatchFunc: func(ctx context.Context, appID int64, adType ad.Type, segmentID int64, version string) (*auction.Config, error) {
 			return auctionConfig, nil
 		},
 		FetchByUIDCachedFunc: func(ctx context.Context, appId int64, key string, aucUID string) *auction.Config {
