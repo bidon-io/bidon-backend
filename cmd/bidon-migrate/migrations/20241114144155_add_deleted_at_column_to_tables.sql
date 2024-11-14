099e5b51337efe894f7af81f46b87eb435b155b1
minisai <antonjyha@gmail.com>
2024-11-14 18:07:26 +0100

    [APDM-810] User can not be deleted

--- cmd/bidon-migrate/migrations/20241114144155_add_deleted_at_column_to_tables.sql
+++ cmd/bidon-migrate/migrations/20241114144155_add_deleted_at_column_to_tables.sql
@@ -5,7 +5,6 @@ ALTER TABLE public.app_demand_profiles ADD COLUMN deleted_at timestamp(6) withou
 ALTER TABLE public.auction_configurations ADD COLUMN deleted_at timestamp(6) without time zone;
 ALTER TABLE public.demand_source_accounts ADD COLUMN deleted_at timestamp(6) without time zone;
 ALTER TABLE public.line_items ADD COLUMN deleted_at timestamp(6) without time zone;
-    ALTER TABLE public.users ADD COLUMN deleted_at timestamp(6) without time zone;
 -- +goose StatementEnd
 
 -- +goose Down
@@ -15,5 +14,4 @@ ALTER TABLE public.app_demand_profiles DROP COLUMN deleted_at;
 ALTER TABLE public.auction_configurations DROP COLUMN deleted_at;
 ALTER TABLE public.demand_source_accounts DROP COLUMN deleted_at;
 ALTER TABLE public.line_items DROP COLUMN deleted_at;
-ALTER TABLE public.users DROP COLUMN deleted_at;
 -- +goose StatementEnd
