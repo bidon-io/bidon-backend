bddffd253c1867c8b63aea5e547e60ef1f5d6f00
Alexander Sikorsky <alexandr.sikorsky@appodeal.com>
2024-11-20 12:33:38 +0100

    [BDN-887] Use redis cache in tests

--- config/redis_cache.go
+++ config/redis_cache.go
@@ -6,13 +6,11 @@ import (
 
 	"github.com/go-redis/cache/v9"
 	"github.com/redis/go-redis/v9"
-	"golang.org/x/sync/singleflight"
 )
 
 type RedisCache[T any] struct {
 	cache *cache.Cache
 	ttl   time.Duration
-	group singleflight.Group
 }
 
 // NewRedisCacheOf initializes the RedisCache with a TTL, and local TinyLFU cache.
@@ -30,38 +28,22 @@ func NewRedisCacheOf[T any](client *redis.Client, ttl time.Duration) *RedisCache
 
 // Get retrieves a value from the cache or loads it using the load function if not found.
 func (c *RedisCache[T]) Get(ctx context.Context, key []byte, load func(ctx context.Context) (T, error)) (T, error) {
-	var zero T
-
-	keyStr := string(key)
-
 	var result T
-	if err := c.cache.Get(ctx, keyStr, &result); err == nil {
-		return result, nil
-	}
 
-	// Use singleflight to prevent redundant fetches
-	v, err, _ := c.group.Do(keyStr, func() (any, error) {
-		freshData, loadErr := load(ctx)
-		if loadErr != nil {
-			return nil, loadErr
-		}
-
-		err := c.cache.Set(&cache.Item{
-			Ctx:   ctx,
-			Key:   keyStr,
-			Value: freshData,
-			TTL:   c.ttl,
-		})
-		if err != nil {
-			return nil, err
-		}
-
-		return freshData, nil
+	err := c.cache.Once(&cache.Item{
+		Ctx:   ctx,
+		Key:   string(key),
+		Value: &result,
+		TTL:   c.ttl,
+		Do: func(*cache.Item) (interface{}, error) {
+			return load(ctx)
+		},
 	})
 
 	if err != nil {
+		var zero T
 		return zero, err
 	}
 
-	return v.(T), nil
+	return result, nil
 }
