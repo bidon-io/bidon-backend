f34b7a30eb16eede67c5378c0b09020061039705
Konstantin Ilchenko <k.ilchenko@appodeal.com>
2024-12-10 11:20:23 +0100

    [BDN-XXX] Add real proxy bidding server

--- proxy/src/bidding/proxy.rs
+++ proxy/src/bidding/proxy.rs
@@ -5,14 +5,24 @@ use crate::com::iabtechlab::openrtb::v3::Openrtb;
 use tonic::transport::Channel;
 use tonic::Request;
 
+#[derive(Debug, Clone)]
 pub struct ProxyBiddingService {
-    grpc_client: BiddingServiceClient<Channel>,
+    grpc_client: Option<BiddingServiceClient<Channel>>,
+    grpc_url: &'static str,
 }
 
 impl ProxyBiddingService {
-    pub async fn new(grpc_url: String) -> Result<Self, Box<dyn std::error::Error>> {
-        let grpc_client = BiddingServiceClient::connect(grpc_url).await?;
-        Ok(ProxyBiddingService { grpc_client })
+    pub fn new(grpc_url: &'static str,) -> Self {
+        ProxyBiddingService {
+            grpc_client: None,
+            grpc_url,
+        }
+    }
+
+    pub async fn connect(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let grpc_client = BiddingServiceClient::connect(self.grpc_url).await?;
+        self.grpc_client = Some(grpc_client);
+        Ok(())
     }
 }
 
@@ -20,8 +30,8 @@ impl ProxyBiddingService {
 impl BiddingService for ProxyBiddingService {
     async fn bid(&self, request: Openrtb) -> Result<Openrtb, BiddingError> {
         let grpc_request = Request::new(request);
-        let grpc_response = self
-            .grpc_client
+        let grpc_client = self.grpc_client.as_ref().ok_or_else(|| BiddingError::new("gRPC client not initialized".to_string()))?;
+        let grpc_response = grpc_client
             .clone() // Cloning is required here, because Tonic gRPC clients are mutable. Cloning is cheap.
             .bid(grpc_request)
             .await
