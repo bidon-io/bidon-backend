81364f342e1567dba6cdffb66d5f9d32247f20cf
minisai <antonjyha@gmail.com>
2025-05-15 13:47:34 +0200

    [APDM-1222] Use s3 instead of minio

--- docker/kafka-connect/create-connector.sh
+++ docker/kafka-connect/create-connector.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-echo "Kafka Connect is ready. Creating S3 Sink Connector..."
+echo "Kafka Connect is ready. Recreating S3 Sink Connector..."
+
+curl -v -X DELETE http://localhost:8083/connectors/s3-sink || true
 
 curl -v -X POST http://localhost:8083/connectors \
   -H "Content-Type: application/json" \
