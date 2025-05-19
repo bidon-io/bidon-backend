7e4ff2e9d508f6c98c7602205af62ec0cfae2773
minisai <antonjyha@gmail.com>
2025-05-19 17:49:37 +0200

    [APDM-1222] Use env variables

--- docker/kafka-connect/create-connector.sh
+++ docker/kafka-connect/create-connector.sh
@@ -4,8 +4,10 @@ set -e
 
 echo "Kafka Connect is ready. Recreating S3 Sink Connector..."
 
+envsubst < /tmp/connector-config.template.json > /tmp/connector-config.json
+
 curl -v -X DELETE http://localhost:8083/connectors/s3-sink || true
 
 curl -v -X POST http://localhost:8083/connectors \
   -H "Content-Type: application/json" \
-  --data "@/scripts/connector-config.json"
+  --data "@/tmp/connector-config.json"
